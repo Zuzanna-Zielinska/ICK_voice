68f0a7ecb17fae492d8dd4c239e39ef51fe39cc5
Zuzanna-Zielinska <72555789+Zuzanna-Zielinska@users.noreply.github.com>
2023-04-14 21:08:36 +0200

    nietestowany rest api

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ICK_voice
 
 Repozytorium do interfejsu komend głosowych na przedmiot Interfejsy człowiek-komputer.
+
+### Użyte biblioteki:
+
+- vosk - modele do rozpoznawania mowy
+- pyaudio - odbieranie dźwięku
+- request - korzystanie z REST API
--- projekt/voice.py
+++ projekt/voice.py
@@ -1,42 +1,70 @@
 #!pip3 install vosk
 #!pip install pyaudio
+# requests
+
 #https://alphacephei.com/vosk/models - modele
 #https://drive.google.com/drive/u/1/folders/1PrRAXjdrNtalk6n-y42_t1iZUexcPRwB - link do modeli na dysku po polsku i angielsku
 
 from vosk import Model, KaldiRecognizer
 import pyaudio
+import requests
     
-model = Model(r"C:\\Users\\kstal\\Downloads\\vosk-model-small-pl-0.22")
+model = Model(r"vosk-model-small-pl-0.22")
 recognizer = KaldiRecognizer(model, 16000)
-    
+
 mic = pyaudio.PyAudio()
 stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
-    
+
+url = "http://localhost:5001"
+info = {"racketDirection": 1, "velocity": 1}
+
 while True:
     data = stream.read(4096)
-        
+
     if recognizer.AcceptWaveform(data):
         text = recognizer.Result()
         if text[14:-3] == "start":
+            info = {"racketDirection": 1, "velocity": 1}
             print("start")
         elif text[14:-3] == "wolniej":
+            info = {"racketDirection": 1, "velocity": 1}
             print("wolniej") #PRĘDKOŚĆ PIŁKI: 1-  wolniej o 10%
         elif text[14:-3] == "szybciej":
+            info = {"racketDirection": 1, "velocity": 1}
             print("szybciej")#PRĘDKOŚĆ PIŁKI: 2-  szybciej o 10%
         elif text[14:-3] == "mniejsze":
-            print("mniejsze") #POLE GRY 1 - zmniejszenie wysokości pola o 10%    
+            info = {"racketDirection": 1, "velocity": 1}
+            print("mniejsze") #POLE GRY 1 - zmniejszenie wysokości pola o 10%
         elif text[14:-3] == "większe":
+            info = {"racketDirection": 1, "velocity": 1}
             print("większe")  #POLE GRY 2 - zwiększenie wysokości pola o 10%
         elif text[14:-3] == "reset":
+            info = {"racketDirection": 1, "velocity": 1}
             print("reset") # TABLICA WYNIKÓW - 1 - reset wyniku
         elif text[14:-3] == "mnożenie":
-            print("mnożenie") # TABLICA WYNIKÓW - 2 - mnożnik bramek 
+            info = {"racketDirection": 1, "velocity": 1}
+            print("mnożenie") # TABLICA WYNIKÓW - 2 - mnożnik bramek
         elif text[14:-3] == "przenieś":
-            print("przenieś") # TELEPORTACJA PIŁKI - oczekiwane wartości położenia docelowego 
+            info = {"racketDirection": 1, "velocity": 1}
+            print("przenieś") # TELEPORTACJA PIŁKI - oczekiwane wartości położenia docelowego
         elif text[14:-3] == ("stop"):
+            info = {"racketDirection": 1, "velocity": 1}
             print("stop")
             break;
         else:
             print("Bad instruction")
             print(f"' {text[14:-3]} '")
+
+        # response = requests.post(url, json=info)
+        # response.json()
+
+        # print(response.status_code)
+
+
+
+
+
+# jeśli response = 201, to znaczy, że wszystko się poprawnie wysłało
+
+
