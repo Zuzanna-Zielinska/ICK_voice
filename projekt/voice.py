ea343f1249dacddf127a5fffe447cfbcc65ce109
Zuzanna Zielińska <zzielinska@student.agh.edu.pl>
2023-04-28 20:53:35 +0200

    sterowanie prędkośćią piłki i resetem wyniku

--- projekt/voice.py
+++ projekt/voice.py
@@ -1,6 +1,6 @@
 #!pip3 install vosk
 #!pip install pyaudio
-# requests
+#!pip install requests
 
 #https://alphacephei.com/vosk/models - modele
 #https://drive.google.com/drive/u/1/folders/1PrRAXjdrNtalk6n-y42_t1iZUexcPRwB - link do modeli na dysku po polsku i angielsku
@@ -8,6 +8,14 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import requests
+
+url = "http://localhost:5001"         # adres serwera
+info = { "id": 1, "scoreboard": 0, "ballVelocity": 1}       # słownik do wysłania
+
+reset_scoreboard = False   # zmienna do resetu wyniku
+velocity = 1              # prędkość piłki
+v_lower_limit = 0        # dolny limit prędkości
+v_upper_limit = 100     # górny limit prędkości
     
 model = Model(r"vosk-model-small-pl-0.22")
 recognizer = KaldiRecognizer(model, 16000)
@@ -16,52 +24,120 @@ mic = pyaudio.PyAudio()
 stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
 
-url = "http://localhost:5001"
-info = {"racketDirection": 1, "velocity": 1}
+
+def send_info(reset_scoreboard: bool, velocity: int):
+    """Funkcja wysyłająca informacje do serwera - wyjście interfejsu"""
+
+    info = { "id": 1, "scoreboard": int(reset_scoreboard), "ballVelocity": velocity}
+
+    # response = requests.post(url, json=info)
+    # response.json()
+    #
+    # print(response.status_code)
+
+    print(info)
 
 while True:
     data = stream.read(4096)
 
     if recognizer.AcceptWaveform(data):
         text = recognizer.Result()
-        if text[14:-3] == "start":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("start")
-        elif text[14:-3] == "wolniej":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("wolniej") #PRĘDKOŚĆ PIŁKI: 1-  wolniej o 10%
-        elif text[14:-3] == "szybciej":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("szybciej")#PRĘDKOŚĆ PIŁKI: 2-  szybciej o 10%
-        elif text[14:-3] == "mniejsze":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("mniejsze") #POLE GRY 1 - zmniejszenie wysokości pola o 10%
-        elif text[14:-3] == "większe":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("większe")  #POLE GRY 2 - zwiększenie wysokości pola o 10%
-        elif text[14:-3] == "reset":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("reset") # TABLICA WYNIKÓW - 1 - reset wyniku
-        elif text[14:-3] == "mnożenie":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("mnożenie") # TABLICA WYNIKÓW - 2 - mnożnik bramek
-        elif text[14:-3] == "przenieś":
-            info = {"racketDirection": 1, "velocity": 1}
-            print("przenieś") # TELEPORTACJA PIŁKI - oczekiwane wartości położenia docelowego
+
+        if text[14:-3] == "reset":
+            print("reset")  # TABLICA WYNIKÓW - 1 - reset wyniku
+            reset_scoreboard = True
+
+            send_info(reset_scoreboard, velocity)
+            reset_scoreboard = False
+
         elif text[14:-3] == ("stop"):
-            info = {"racketDirection": 1, "velocity": 1}
             print("stop")
+
             break;
+
+        elif text[14:-3] == ("szybciej"):
+            print("szybciej")
+            if velocity < v_upper_limit: #blokada przekroczenia górnego limitu
+                velocity += 1
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("wolniej"):
+            print("wolniej")
+            if velocity > v_lower_limit: #blokada przekroczenia dolnego limitu
+                velocity -= 1
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("zero"):
+            print("zero")
+            velocity = 0
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("pięć"):
+            print("pięć")
+            velocity = 5
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("dziesięć"):
+            print("dziesięć")
+            velocity = 10
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("piętnaście"):
+            print("piętnaście")
+            velocity = 15
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("dwadzieścia"):
+            print("dwadzieścia")
+            velocity = 20
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("dwadzieścia pięć"):
+            print("dwadzieścia pięć")
+            velocity = 25
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("trzydzieści"):
+            print("trzydzieści")
+            velocity = 30
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("trzydzieści pięć"):
+            print("trzydzieści pięć")
+            velocity = 35
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("czterdzieści"):
+            print("czterdzieści")
+            velocity = 40
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("czterdzieści pięć"):
+            print("czterdzieści pięć")
+            velocity = 45
+
+            send_info(reset_scoreboard, velocity)
+
+        elif text[14:-3] == ("pięćdziesiąt"):
+            print("pięćdziesiąt")
+            velocity = 50
+
+            send_info(reset_scoreboard, velocity)
+
         else:
             print("Bad instruction")
             print(f"' {text[14:-3]} '")
-
-        # response = requests.post(url, json=info)
-        # response.json()
-
-        # print(response.status_code)
-
-
 
 
 
