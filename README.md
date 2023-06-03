c17f3a217b609ca677490aa38273e53ea8158bf2
Zuzanna Zielińska <zzielinska@student.agh.edu.pl>
2023-06-03 13:14:49 +0200

    poprawa czytelności

--- README.md
+++ README.md
@@ -12,9 +12,3 @@ Repozytorium do interfejsu komend głosowych na przedmiot Interfejsy człowiek-k
 
 Skrypt uruchamiający rozpoznawanie komend głosowych to plik voice.py w folderze projekt.
 
-Domyślnie projekt nie wysyła nigdzie informacji, tylko rejestruje komendy, zmienia wartości danych do wysłania oraz oba wypisuje.
-Do uruchoienia wysyłania danych należy:
-
- - w linijce 12 wpisać odpowiedni adres serwera,
- - w linijkach 33 - 36 odkomentować kod wysyłający dane do serwera,
- - w linijkach 53 - 56 zakomentować kod zatrzymujący nasłuchiwanie komend za pomocą słowa "stop". Polecenie jest potrzebne do testów i będzie negatywnie wypływało na doświadczenie sterwoania grą, jeśli trafi do gotowej aplikacji.
--- projekt/voice.py
+++ projekt/voice.py
@@ -9,14 +9,15 @@ from vosk import Model, KaldiRecognizer
 import pyaudio
 import requests
 
-url = "http://localhost:5001"         # adres serwera
+url = "http://192.168.1.150:5001/"         # adres serwera
 info = { "id": 1, "scoreboard": 0, "ballVelocity": 1}       # słownik do wysłania
+is_connected_to_server = False # zmienna określająca, czy funkcja jest używana do sterowania grą (True), czy testowania komend (False)
 
 reset_scoreboard = False   # zmienna do resetu wyniku
 velocity = 1              # prędkość piłki
-v_lower_limit = 0        # dolny limit prędkości
-v_upper_limit = 50     # górny limit prędkości
-    
+v_lower_limit = 1        # dolny limit prędkości
+v_upper_limit = 100     # górny limit prędkości
+
 model = Model(r"vosk-model-small-pl-0.22")
 recognizer = KaldiRecognizer(model, 16000)
 
@@ -25,127 +26,189 @@ stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, fr
 stream.start_stream()
 
 
-def send_info(reset_scoreboard: bool, velocity: int):
+def send_info(reset_scoreboard: bool, velocity: int, is_connected_to_server:bool = False):
     """Funkcja wysyłająca informacje do serwera - wyjście interfejsu"""
 
-    info = { "id": 1, "scoreboard": int(reset_scoreboard), "ballVelocity": velocity}
+    if is_connected_to_server:
+        info = { "id": 1, "scoreboard": int(reset_scoreboard), "ballVelocity": velocity}
+        response = requests.post(url, json=info)
 
-    # response = requests.post(url, json=info)
-    # response.json()
-    #
-    # print(response.status_code)
+def recognise_commands(is_connected_to_server, reset_scoreboard, velocity, v_lower_limit, v_upper_limit):
+    """Funkcja rozpoznająca komendy głosowe i wysyłająca informacje do serwera
 
-    print(info)
+    is_connected_to_server  # zmienna określająca, czy funkcja jest używana do sterowania grą (True), czy testowania komend (False)
+    reset_scoreboard       # zmienna do resetu wyniku
+    velocity              # prędkość piłki
+    v_lower_limit        # dolny limit prędkości
+    v_upper_limit       # górny limit prędkości
+    """
 
-while True:
-    data = stream.read(4096)
+    print("Nasłuchuję!")
+    while True:
+        data = stream.read(4096)
 
-    if recognizer.AcceptWaveform(data):
-        text = recognizer.Result()
+        if recognizer.AcceptWaveform(data):
+            text = recognizer.Result()
 
-        if text[14:-3] == "reset":
-            print("reset")  # TABLICA WYNIKÓW - 1 - reset wyniku
-            reset_scoreboard = True
+            if text[14:-3] == "reset":
+                print("reset")  # TABLICA WYNIKÓW - 1 - reset wyniku
+                reset_scoreboard = True
 
-            send_info(reset_scoreboard, velocity)
-            reset_scoreboard = False
+                send_info(reset_scoreboard, velocity)
+                reset_scoreboard = False
 
-        elif text[14:-3] == ("stop"):
-            print("stop")
+            elif text[14:-3] == ("stop") and not is_connected_to_server:
+                print("stop")
 
-            break;
+                break;
 
-        elif text[14:-3] == ("szybciej"):
-            print("szybciej")
-            if velocity < v_upper_limit: #blokada przekroczenia górnego limitu
-                velocity += 1
+            elif text[14:-3] == ("szybciej"):
+                print("szybciej")
+                if velocity < v_upper_limit: #blokada przekroczenia górnego limitu
+                    velocity += 1
+                else:
+                    print("NIE MOŻESZ JUŻ PRZYSPIESZYĆ!")
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("wolniej"):
+                print("wolniej")
+                if velocity > v_lower_limit: #blokada przekroczenia dolnego limitu
+                    velocity -= 1
+                else:
+                    print("NIE MOŻESZ JUŻ ZWOLNIĆ!")
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("jeden"):
+                print("jeden")
+                velocity = 1
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("dwa"):
+                print("dwa")
+                velocity = 2
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("trzy"):
+                print("trzy")
+                velocity = 3
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("cztery"):
+                print("cztery")
+                velocity = 4
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("pięć"):
+                print("pięć")
+                velocity = 5
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("sześć"):
+                print("sześć")
+                velocity = 6
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("siedem"):
+                print("siedem")
+                velocity = 7
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("osiem"):
+                print("osiem")
+                velocity = 8
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("dziewięć"):
+                print("dziewięć")
+                velocity = 9
+
+                send_info(reset_scoreboard, velocity)
+
+            elif text[14:-3] == ("dziesięć"):
+                print("dziesięć")
+                velocity = 10
+
+                send_info(reset_scoreboard, velocity)
+
+
+            # elif text[14:-3] == ("zero"):
+            #     print("zero")
+            #     velocity = 0
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("pięć"):
+            #     print("pięć")
+            #     velocity = 5
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("dziesięć"):
+            #     print("dziesięć")
+            #     velocity = 10
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("piętnaście"):
+            #     print("piętnaście")
+            #     velocity = 15
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("dwadzieścia"):
+            #     print("dwadzieścia")
+            #     velocity = 20
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("dwadzieścia pięć"):
+            #     print("dwadzieścia pięć")
+            #     velocity = 25
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("trzydzieści"):
+            #     print("trzydzieści")
+            #     velocity = 30
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("trzydzieści pięć"):
+            #     print("trzydzieści pięć")
+            #     velocity = 35
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("czterdzieści"):
+            #     print("czterdzieści")
+            #     velocity = 40
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("czterdzieści pięć"):
+            #     print("czterdzieści pięć")
+            #     velocity = 45
+            #
+            #     send_info(reset_scoreboard, velocity)
+            #
+            # elif text[14:-3] == ("pięćdziesiąt"):
+            #     print("pięćdziesiąt")
+            #     velocity = 50
+            #
+            #     send_info(reset_scoreboard, velocity)
+
             else:
-                print("NIE MOŻESZ JUŻ PRZYSPIESZYĆ!")
+                print("Bad instruction")
+                print(f"' {text[14:-3]} '")
 
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("wolniej"):
-            print("wolniej")
-            if velocity > v_lower_limit: #blokada przekroczenia dolnego limitu
-                velocity -= 1
-            else:
-                print("NIE MOŻESZ JUŻ ZWOLNIĆ!")
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("zero"):
-            print("zero")
-            velocity = 0
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("pięć"):
-            print("pięć")
-            velocity = 5
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("dziesięć"):
-            print("dziesięć")
-            velocity = 10
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("piętnaście"):
-            print("piętnaście")
-            velocity = 15
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("dwadzieścia"):
-            print("dwadzieścia")
-            velocity = 20
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("dwadzieścia pięć"):
-            print("dwadzieścia pięć")
-            velocity = 25
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("trzydzieści"):
-            print("trzydzieści")
-            velocity = 30
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("trzydzieści pięć"):
-            print("trzydzieści pięć")
-            velocity = 35
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("czterdzieści"):
-            print("czterdzieści")
-            velocity = 40
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("czterdzieści pięć"):
-            print("czterdzieści pięć")
-            velocity = 45
-
-            send_info(reset_scoreboard, velocity)
-
-        elif text[14:-3] == ("pięćdziesiąt"):
-            print("pięćdziesiąt")
-            velocity = 50
-
-            send_info(reset_scoreboard, velocity)
-
-        else:
-            print("Bad instruction")
-            print(f"' {text[14:-3]} '")
-
-
-
-# jeśli response = 201, to znaczy, że wszystko się poprawnie wysłało
-
-
-
+recognise_commands(is_connected_to_server, reset_scoreboard, velocity, v_lower_limit, v_upper_limit)
